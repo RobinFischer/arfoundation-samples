5bb8cec54ce1bb2f34cd66e12d7f902c06e626c9
RobinFischer <robin.fischer7@web.de>
2020-06-09 19:02:03 +0800

    add HotdogTest code
    barracuda test: https://github.com/mantasp/Barracuda-HotdogTest

--- Assets/Scripts/TestCameraImage.cs
+++ Assets/Scripts/TestCameraImage.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+using Unity.Barracuda;
+using UnityEngine.Profiling;
+using System.Collections;
+using System.Collections.Generic;
+
 /// <summary>
 /// This component tests getting the latest camera image
 /// and converting it to RGBA format. If successful,
@@ -33,7 +38,34 @@ public class TestCameraImage : MonoBehaviour
     [Tooltip("The ARCameraManager which will produce frame events.")]
     ARCameraManager m_CameraManager;
 
-     /// <summary>
+    // Baracuda
+    public float epsilon = 1e-3f;
+    public NNModel srcModel;
+    //spublic Texture2D inputImage;
+    public TextAsset labelsAsset;
+    public int inputResolutionY = 224;
+    public int inputResolutionX = 224;
+    public Material preprocessMaterial;
+
+    public bool useGPU = true;
+
+    private string imgInfo;
+    //public RawImage displayImage;
+    private Texture2D m_barracudaTexture;
+    public Button m_YourFirstButton;
+
+    //public int repeatExecution = 1000000;
+
+    private Model model;
+    private IWorker engine;
+    private Dictionary<string, Tensor> inputs = new Dictionary<string, Tensor>();
+    private string[] labels;
+
+    private float averageDt;
+    private float rawAverageDt;
+    private bool flagFrameRdy;
+
+    /// <summary>
     /// Get or set the <c>ARCameraManager</c>.
     /// </summary>
     public ARCameraManager cameraManager
@@ -81,7 +113,32 @@ public class TestCameraImage : MonoBehaviour
             m_CameraManager.frameReceived -= OnCameraFrameReceived;
         }
     }
+    IEnumerator Start()
+    {
+        //workaround button
+        m_YourFirstButton.onClick.AddListener(TaskOnClick);
 
+        Application.targetFrameRate = 60;
+
+        labels = labelsAsset.text.Split('\n');
+        model = ModelLoader.Load(srcModel, false);
+        engine = WorkerFactory.CreateWorker(model, useGPU ? WorkerFactory.Device.GPU : WorkerFactory.Device.CPU);
+
+        //var input = new Tensor(PrepareTextureForInput(m_Texture, !useGPU), 3);
+
+        //inputs["input"] = input;
+
+        yield return null;
+
+        flagFrameRdy = true;
+        StartCoroutine(RunInference());
+    }
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+        flagFrameRdy = true;
+    }
     unsafe void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
     {
         // Attempt to get the latest camera image. If this method succeeds,
@@ -91,9 +148,9 @@ public class TestCameraImage : MonoBehaviour
         {
             return;
         }
-
+  
         // Display some information about the camera image
-        m_ImageInfo.text = string.Format(
+        imgInfo = string.Format(
             "Image info:\n\twidth: {0}\n\theight: {1}\n\tplaneCount: {2}\n\ttimestamp: {3}\n\tformat: {4}",
             image.width, image.height, image.planeCount, image.timestamp, image.format);
 
@@ -122,6 +179,8 @@ public class TestCameraImage : MonoBehaviour
         try
         {
             image.Convert(conversionParams, new IntPtr(rawTextureData.GetUnsafePtr()), rawTextureData.Length);
+
+
         }
         finally
         {
@@ -135,7 +194,104 @@ public class TestCameraImage : MonoBehaviour
 
         // Set the RawImage's texture so we can visualize it.
         m_RawImage.texture = m_Texture;
+        m_barracudaTexture = m_Texture; //copy to prevent simultanious access of texture (but apparently not the reason for the app crashing)
     }
 
+    Texture PrepareTextureForInput(Texture2D src, bool needsCPUcopy)
+    {
+        var targetRT = RenderTexture.GetTemporary(inputResolutionX, inputResolutionY, 0, RenderTextureFormat.ARGB32);
+        RenderTexture.active = targetRT;
+        Graphics.Blit(src, targetRT, preprocessMaterial);
+
+        if (!needsCPUcopy)
+            return targetRT;
+
+        var result = new Texture2D(targetRT.width, targetRT.height);
+        result.ReadPixels(new Rect(0, 0, targetRT.width, targetRT.height), 0, 0);
+        result.Apply();
+
+        return result;
+    }
+    IEnumerator RunInference()
+    {
+        // Skip frame before starting
+        //yield return null;
+        //m_RawImage.texture = m_Texture;
+
+        while (true)//repeatExecution-- > 0)
+        {
+            if (flagFrameRdy)
+            {
+                var input = new Tensor(PrepareTextureForInput(m_barracudaTexture, !useGPU), 3);
+
+                inputs["input"] = input;
+
+                //yield return null;
+
+                try
+                {
+                    var start = Time.realtimeSinceStartup;
+
+                    Profiler.BeginSample("Schedule execution");
+                    engine.Execute(inputs);
+                    Profiler.EndSample();
+
+                    Profiler.BeginSample("Fetch execution results");
+                    var output = engine.PeekOutput();
+                    Profiler.EndSample();
+
+                    var res = output.ArgMax()[0];
+                    var end = Time.realtimeSinceStartup;
+                    var label = labels[res];
+
+                    if (label.Contains("hotdog") && Mathf.Abs(output[res] - 0.578f) < epsilon)
+                    {
+                        m_ImageInfo.color = Color.green;
+                        m_ImageInfo.text = $"Success: {labels[res]} {output[res] * 100}% \n" + imgInfo;
+                    }
+                    else
+                    {
+                        m_ImageInfo.color = Color.red;
+                        m_ImageInfo.text = $"Failed: {labels[res]} {output[res] * 100}% \n" + imgInfo;
+                    }
+
+                    UpdateAverage(end - start);
+                    Debug.Log($"frametime = {(end - start) * 1000f}ms, average = {averageDt * 1000}ms");
+
+
+                }
+                catch (Exception e)
+                {
+                    Debug.Log($"Exception happened {e}");
+                    //throw;
+                }
+                flagFrameRdy = false;
+
+            }
+
+            yield return null;
+        }
+    }
+    private void UpdateAverage(float newValue)
+    {
+        rawAverageDt = rawAverageDt * 0.9f + 0.1f * newValue;
+
+        // Drop spikes above 20%
+        if (newValue < 1.2f * rawAverageDt)
+        {
+            averageDt = averageDt * 0.9f + 0.1f * newValue;
+        }
+    }
+    private void OnDestroy()
+    {
+        engine?.Dispose();
+
+        foreach (var key in inputs.Keys)
+        {
+            inputs[key].Dispose();
+        }
+
+        inputs.Clear();
+    }
     Texture2D m_Texture;
 }
